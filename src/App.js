3a4c66bb607cf784164cb0bc9d28e77f9874721c
Vinicius Gonzaga Guilherme <1401143@sga.pucminas.br>
2023-02-14 10:16:54 -0300

    new cors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express();
+const cors = require("cors");
 const functionary = require("./routes/functionaryRouter");
 const occupation = require("./routes/occupationRouter");
 
@@ -8,6 +9,7 @@ const PORT = 3000;
 
 const bodyParser = require("body-parser");
 
+app.use(cors());
 app.use(bodyParser.json());
 
 
